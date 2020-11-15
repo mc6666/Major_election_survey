e696d5b135541a8218d44b41f9a4c42a82f7c5dc
mc6666 <mc6666@gmail.com>
2020-11-15 10:40:41 +0800

    v2

--- Major_election_survey.py
+++ Major_election_survey.py
@@ -35,7 +35,7 @@ def draw_chart(df):
     # ax.set_xlabel('候選人', fontproperties=fontProperties)
     # ax.set_xticks(list(range(df['投給候選人'].nunique())))
     # ax.set_xticklabels(df['投給候選人'].unique()) #, fontproperties=fontProperties)
-    ax.set_xticklabels([i for i in range(df['投給候選人'].nunique())) #, fontproperties=fontProperties)
+    ax.set_xticklabels([i for i in range(len(df['投給候選人'].unique()))) #, fontproperties=fontProperties)
     st.pyplot(ax.figure)
     
 def check_input():
